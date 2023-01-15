f260ee0b2d998834741815547f5a89e4e9cc661d
Osama Afify <Osama.afify@outlook.com>
2023-01-15 23:02:28 +0200

    15-1-2023

--- Recursion-Fibonacci.cpp
+++ Recursion-Fibonacci.cpp
@@ -14,14 +14,51 @@ long double fibo(int x){
 
 }
 
+int to_power(int x, int y){
+    int z;
+    if (y == 0)
+        return 1;
+    z = to_power(x, y/2);
+    z *= z;
+
+    if (y%2 != 0)
+        z *= x;
+    return z;
+
+
+}
+
 int main(){
-    int in_num;
+
+    int in_num, in_power;
     long double nout;
-    printf("Please Enter a number to git its Fibonacci: ");
+    char in_function;
+    do{
+    cout<<"Please Select the Function. (Fibonacci \"Type f\" or Power \"Type p\": ";
+    cin>>in_function;
+    cout<<"\n";
 
-    cin>>in_num;
+    if (in_function == 'f'){
+        printf("Please Enter a number to git its Fibonacci: ");
+
+        cin>>in_num;
 
 
-    nout = fibo(in_num);
-    cout<< nout<<endl;
+        nout = fibo(in_num);
+        cout<< nout<<endl;
+    }
+    else if (in_function == 'p'){
+        printf("Please Enter the Main Number: ");
+        cin>>in_num;
+        printf("\nPlease Enter the Power: ");
+        cin>>in_power;
+
+        nout = to_power(in_num, in_power);
+        cout<<"\n";
+
+        cout<< nout<<endl;
+
+
+    }
+    }while(in_function != 'f' && in_function != 'p' );
 }
